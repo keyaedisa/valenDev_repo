41a0bfe9f282941fb9aa41393defc408dbd0ef0b
keyaedisa <46032999+keyaedisa@users.noreply.github.com>
2022-11-13 15:22:03 -0500

    update readme and install script for dev repo

--- install/addRepo2PacmanConf.sh
+++ install/addRepo2PacmanConf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Automatically adding Valen Repo to your /etc/pacman.conf"
+echo "Automatically adding ValenDev Repo to your /etc/pacman.conf"
 sed -n '11,14p' README.md >> /etc/pacman.conf
 echo "Updating pacman databases"
 pacman -Sy
