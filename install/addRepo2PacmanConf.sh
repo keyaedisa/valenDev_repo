797e449a8aaee52d0f4b05bbe343f1e77811534d
keyaedisa <46032999+keyaedisa@users.noreply.github.com>
2023-01-29 20:31:23 -0500

    test

--- install/addRepo2PacmanConf.sh
+++ install/addRepo2PacmanConf.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Automatically adding Valen Dev Repo to your /etc/pacman.conf"
-wget https://raw.githubusercontent.com/keyaedisa/valenDev_repo/master/README.md
+wget https://raw.githubusercontent.com/keyaedisa/valenDev_repo/trunk/README.md
 sed -n '19,22p' README.md >> /etc/pacman.conf
 rm README.md
 echo "Updating pacman databases"
-wget https://raw.githubusercontent.com/keyaedisa/valenDev_repo/master/install/repo-Arch-public-key.gpg
+wget https://raw.githubusercontent.com/keyaedisa/valenDev_repo/trunk/install/repo-Arch-public-key.gpg
 pacman-key -a repo-Arch-public-key.gpg
 pacman-key --finger w0rldEater
 pacman-key --lsign-key w0rldEater
