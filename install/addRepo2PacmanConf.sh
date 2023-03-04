08183d4ea18385270d085593fb6a2b1710ecff4c
keyaedisa <46032999+keyaedisa@users.noreply.github.com>
2023-03-04 02:16:09 -0500

    reapri

--- install/addRepo2PacmanConf.sh
+++ install/addRepo2PacmanConf.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
-echo "Automatically adding Valen Dev Repo to your /etc/pacman.conf"
-wget https://raw.githubusercontent.com/keyaedisa/valenDev_repo/trunk/README.md
-sed -n '12,15p' README.md >> /etc/pacman.conf
-rm README.md
-echo "Updating pacman databases"
-wget https://raw.githubusercontent.com/keyaedisa/valenDev_repo/trunk/install/repo-Arch-public-key.gpg
-pacman-key -a repo-Arch-public-key.gpg
-pacman-key --finger w0rldEater
-pacman-key --lsign-key w0rldEater
-rm repo-Arch-public-key.gpg
-pacman -Sy
+cd ~/
+
+if [[ "$1" == "dev" ]]; then
+	echo "Automatically adding Valen Repo to your /etc/pacman.conf"
+	curl -s -o README.md https://raw.githubusercontent.com/keyaedisa/valenDev_repo/master/README.md
+	sed -n '19,22p' README.md >> /etc/pacman.conf
+	rm README.md
+	echo "Updating pacman databases"
+	curl -s -o repo-Arch-public-key.gpg https://raw.githubusercontent.com/keyaedisa/valenDev_repo/master/install/repo-Arch-public-key.gpg
+	pacman-key -a repo-Arch-public-key.gpg
+	pacman-key --finger w0rldEater
+	pacman-key --lsign-key w0rldEater
+	rm repo-Arch-public-key.gpg
+	pacman -Sy
+else
+	echo "Automatically adding Valen Repo to your /etc/pacman.conf"
+	curl -s -o README.md https://raw.githubusercontent.com/keyaedisa/valenDev_repo/master/README.md
+	sed -n '12,15p' README.md >> /etc/pacman.conf
+	rm README.md
+	echo "Updating pacman databases"
+	curl -s -o repo-Arch-public-key.gpg https://raw.githubusercontent.com/keyaedisa/valenDev_repo/master/install/repo-Arch-public-key.gpg
+	pacman-key -a repo-Arch-public-key.gpg
+	pacman-key --finger w0rldEater
+	pacman-key --lsign-key w0rldEater
+	rm repo-Arch-public-key.gpg
+	pacman -Sy
+fi
